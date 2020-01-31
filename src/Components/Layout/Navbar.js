f01e5b054bebbe21b9a12369396b83ef8a75830d
Sushant Bansal <sushantbansal2@gmail.com>
2020-02-01 00:30:44 +0530

    Updated Navbar Layout, added secondary bar for mobile layout

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -4,9 +4,13 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Github from "@material-ui/icons/GitHub";
 import PropTypes from "prop-types";
-import UserSearch from "../User/UserSearch";
 import Grid from "@material-ui/core/Grid";
+import Hidden from "@material-ui/core/Hidden";
 import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+
+import About from "../Pages/About";
+import UserSearch from "../User/UserSearch";
 
 const Navbar = ({ title, searchUsers, clearSearch }) => {
   return (
@@ -16,7 +20,7 @@ const Navbar = ({ title, searchUsers, clearSearch }) => {
     >
       <Toolbar>
         <Grid container direction='row' alignItems='center'>
-          <Grid item xs={5} sm={6}>
+          <Grid item xs={6} sm={4}>
             <Grid container direction='row' alignItems='center' link=''>
               <Github />
               <Typography variant='h6' style={{ margin: 4 }}>
@@ -24,18 +28,30 @@ const Navbar = ({ title, searchUsers, clearSearch }) => {
               </Typography>
             </Grid>
           </Grid>
-          <Grid item xs={7} sm={6}>
-            <Grid container direction='row' alignItems='center'>
-              <Grid item xs={9} md={11}>
-                <UserSearch searchUsers={searchUsers}></UserSearch>
-              </Grid>
-              <Grid item xs={3} md={1}>
-                <Button onClick={clearSearch}>Clear</Button>
-              </Grid>
+          <Hidden smDown>
+            <Grid item sm={4}>
+              <UserSearch
+                searchUsers={searchUsers}
+                clearSearch={clearSearch}
+              ></UserSearch>
             </Grid>
+          </Hidden>
+          <Grid container xs={6} sm={4} justify='flex-end'>
+            <Button>About</Button>
+            <Button>Menu</Button>
           </Grid>
         </Grid>
       </Toolbar>
+      <Hidden smUp>
+        <Toolbar>
+          <Box width={1}>
+            <UserSearch
+              searchUsers={searchUsers}
+              clearSearch={clearSearch}
+            ></UserSearch>
+          </Box>
+        </Toolbar>
+      </Hidden>
     </AppBar>
   );
 };
