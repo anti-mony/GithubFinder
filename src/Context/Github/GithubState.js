35b44e5652100de99e662926c1fb6e7f9cce5b9a
Sushant Bansal <sushantbansal2@gmail.com>
2020-03-16 00:16:53 -0500

    Added Context using ContextAPI

---
+++ src/Context/Github/GithubState.js
@@ -0,0 +1,80 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import GithubContext from "./githubContext";
+import GithubReducer from "./githubReducer";
+
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USER,
+  GET_USER,
+  GET_REPOS
+} from "../types";
+
+const GithubState = props => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Clear Search / HomePage
+  const clearSearch = async () => {
+    setLoading();
+    dispatch({ type: CLEAR_USER, payload: [] });
+  };
+
+  // Get all Users / Search Users
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({ type: SEARCH_USERS, payload: res.data.items });
+  };
+
+  // Get a single user
+  const getUser = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: GET_USER, payload: res.data });
+  };
+
+  // Get User Repositories
+  const getUserRepos = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: GET_REPOS, payload: res.data });
+  };
+
+  // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+        searchUsers,
+        clearSearch,
+        getUser,
+        getUserRepos
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubState;
---
+++ src/Context/types.js
@@ -0,0 +1,5 @@
+export const SEARCH_USERS = "SEACRH_USERS";
+export const GET_USER = "GET_USER";
+export const CLEAR_USER = "CLEAR_USER";
+export const GET_REPOS = "GET_REPOS";
+export const SET_LOADING = "SET_LOADING";
